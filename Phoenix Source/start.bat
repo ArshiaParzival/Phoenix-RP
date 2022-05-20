e77a2426d342ad562c774bc81baf880b5919c879
Parzival <105792902+ArshiaParzival@users.noreply.github.com>
2022-05-20 16:51:41 +0430

    Add files via upload

---
+++ Phoenix Source/start.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Administrator\Desktop\Phoenix\server-data
+C:\Users\Administrator\Desktop\Phoenix\server-files\run.cmd +exec server.cfg +set sv_enforceGameBuild 2189 +set onesync on +set onesync_enableInfinity 1
