965857ca619d1a5a851961fc94ed27e2ae31a5ee
Parzival <105792902+ArshiaParzival@users.noreply.github.com>
2022-05-21 12:57:13 +0430

    Delete Phoenix Source/[casino] directory

--- Phoenix Source/[casino]/casino_cashier/config.lua
+++
@@ -1,4 +0,0 @@
-Config = {}
-
-Config.ticket = 10000 -- cost ($) fo the lucky wheel ticket
-Config.dollar = 'bank' -- pick (bank,money,cash) location add/remove $$ from : 'bank' = it will take directly from your bank account
